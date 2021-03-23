b1549e30dbe3841bf3e2d8cad73ec8576b361782
Novikov_A <novikov_andrey.7@mail.ru>
2021-03-23 14:13:58 +0300

    выполнил задание к уроку№5

---
+++ LESSON_5/script.js
@@ -0,0 +1,64 @@
+const API_URL = '/goods.json';
+
+const vue = new Vue({
+    el: "#app",
+    data: {
+        goods: [],
+        filtredGoods: [],
+        search: ''
+    },
+    methods: {
+        searchHandler() {
+            if (this.search === '') {
+                this.filtredGoods = this.goods;
+            }
+            const regexp = new RegExp(this.search, 'gi');
+            this.filtredGoods = this.goods.filter((good) => regexp.test(good.title));
+        },
+
+        fetch(error, success) {
+
+            let xhr;
+
+            if (window.XMLHttpRequest) {
+                xhr = new XMLHttpRequest();
+            } else if (window.ActiveXObject) {
+                xhr = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4) {
+                    if (xhr.status === 200) {
+                        success(JSON.parse(xhr.responseText));
+                    } else if (xhr.status > 400) {
+                        error('все пропало');
+                    }
+                }
+            }
+
+            xhr.open('GET', API_URL, true);
+            xhr.send();
+        },
+        fetchPromise() {
+
+            return new Promise((resolve, reject) => {
+                this.fetch(reject, resolve)
+            })
+        }
+    },
+    mounted() {
+        this.fetchPromise()
+            .then(data => {
+                this.goods = data;
+                this.filtredGoods = data;
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+}
+
+})
+
+// задание 3* заглушка для пустого списка товаров 
+//добавил заголовок, в index.html <h1 v-for="filtredGoods.length < 1">"Все пропало"</h1>
